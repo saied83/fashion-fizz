2b12598d56edc70d0fc8835bafcec8923168019f
saied83 <saiedtechit@gmail.com>
2024-08-30 02:02:10 +0600

    complete order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,79 @@
+import { useShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import { useEffect, useState } from "react";
 const Orders = () => {
-  return <div>Orders</div>;
+  const { products, currency, cartItems } = useShopContext();
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1="MY" text2="ORDERS" />
+      </div>
+      <div>
+        {cartData.map((item, indx) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+
+          return (
+            <div
+              key={indx}
+              className="py-4 border-t text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+            >
+              <div className="flex items-start gap-6 text-sm ">
+                <img
+                  src={productData.image[0]}
+                  className="w-16 sm:w-20"
+                  alt=""
+                />
+                <div>
+                  <p className="sm:text-base font-medium">{productData.name}</p>
+                  <div className="flex items-center gap-3 text-base text-gray-700">
+                    <p className="text-lg">
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Size: {item.size}</p>
+                  </div>
+
+                  <p className="mt-2">
+                    Date: <span className="text-gray-400">25 July, 2024</span>
+                  </p>
+                </div>
+              </div>
+              <div className="md:w-1/2 flex justify-between">
+                <div className="flex items-center gap-2">
+                  <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                  <p className="text-sm md:text-base">Ready to ship</p>
+                </div>
+                <button className="border px-4 py-2 text-sm font-medium rounded-sm">
+                  Track Order
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
