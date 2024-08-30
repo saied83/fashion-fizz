e7feac4234cdac1f61608045be6acbe2ab8f4fd8
saied83 <saiedtechit@gmail.com>
2024-08-30 19:01:23 +0600

    complete about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <div className="bg-[#f4f4f4] px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] mt-10 p-5">
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm ">
@@ -15,8 +18,8 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li>Home</li>
-            <li>About us</li>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li onClick={() => navigate("/about")}>About us</li>
             <li>Delivery</li>
             <li>Privacy Policy</li>
           </ul>
